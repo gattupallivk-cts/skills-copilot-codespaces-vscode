aefe73b58b2b52eacb72ec87d3e69d791fad2f50
gattupallivk-cts <87705380+gattupallivk-cts@users.noreply.github.com>
2024-04-01 18:13:30 +0000

    Copilot third commit with new suggestions

--- comments.js
+++ comments.js
@@ -1,50 +1,20 @@
 // create web server
-// create web server
+// to run this use: node comments.js
+// then open a browser and go to: http://localhost:3000
+
 var http = require('http');
 var fs = require('fs');
-var url = require('url');
-var path = require('path');
-var port = 3000;
-var mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css"
-};
-// create web server
-http.createServer(function(req, res){
-    // get file name
-    var uri = url.parse(req.url).pathname;
-    // get file path
-    var fileName = path.join(process.cwd(), unescape(uri));
-    console.log('Loading ' + uri);
-    var stats;
-    try {
-        // check file path
-        stats = fs.lstatSync(fileName);
-    } catch(e) {
-        res.writeHead(404, {'Content-type': 'text/plain'});
-        res.write('404 Not Found\n');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('File not found!');
+        } else {
+            res.write(data);
+        }
         res.end();
-        return;
-    }
-    // check file/directory
-    if(stats.isFile()) {
-        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
-        res.writeHead(200, {'Content-type': mimeType});
-        var fileStream = fs.createReadStream(fileName);
-        fileStream.pipe(res);
-    } else if(stats.isDirectory()) {
-        res.writeHead(302, {
-            'Location': 'index.html'
-        });
-        res.end();
-    } else {
-        res.writeHead(500, {'Content-type': 'text/plain'});
-        res.write('500 Internal Error\n');
-        res.end();
-    }
-}).listen(port);
-console.log('Server running at http://  localhost:' + port + '/');
+    });
+}).listen(3000);
+console.log('Server started at http://localhost:3000');
