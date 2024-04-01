db9db60569626d4722969452d60e77440bf65a02
gattupallivk-cts <87705380+gattupallivk-cts@users.noreply.github.com>
2024-04-01 18:10:12 +0000

    Copilot third commit-corrected the error

--- comments.js
+++ comments.js
@@ -1,57 +1,50 @@
 // create web server
-// load http module
+// create web server
 var http = require('http');
-var url = require('url');
 var fs = require('fs');
-var qs = require('querystring');
-var mysql = require('mysql');
-
-// create http server
-http.createServer( function (request, response) {  
-   // parse the request containing file name
-   var pathname = url.parse(request.url).pathname;
-   var query = url.parse(request.url).query;
-   var queryData = qs.parse(query);
-   var comment = queryData.comment;
-   
-   var con = mysql.createConnection({
-	   host: "localhost",
-	   user: "root",
-	   password: "root",
-	   database: "test"
-   });
-   
-   con.connect(function(err) {
-	   if (err) throw err;
-	   console.log("Connected to database!");
-	   var sql = "INSERT INTO comments (comment) VALUES ('"+comment+"')";
-	   con.query(sql, function (err, result) {
-		   if (err) throw err;
-		   console.log("1 record inserted");
-	   });
-   });
-   
-   // print the name of the file for which request is made
-   console.log("Request for " + pathname + " received.");
-   // from the file system
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else { 
-         //Page found	  
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});	
-         // Write the content of the file to response body
-         response.write(data.toString());		
-      }
-      // Send the response body 
-      response.end();
-   });   
-}).listen(8080);
-
-// Console will print the message
-console.log('Server running at http://');
+var url = require('url');
+var path = require('path');
+var port = 3000;
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"
+};
+// create web server
+http.createServer(function(req, res){
+    // get file name
+    var uri = url.parse(req.url).pathname;
+    // get file path
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log('Loading ' + uri);
+    var stats;
+    try {
+        // check file path
+        stats = fs.lstatSync(fileName);
+    } catch(e) {
+        res.writeHead(404, {'Content-type': 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+    // check file/directory
+    if(stats.isFile()) {
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+        res.writeHead(200, {'Content-type': mimeType});
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    } else if(stats.isDirectory()) {
+        res.writeHead(302, {
+            'Location': 'index.html'
+        });
+        res.end();
+    } else {
+        res.writeHead(500, {'Content-type': 'text/plain'});
+        res.write('500 Internal Error\n');
+        res.end();
+    }
+}).listen(port);
+console.log('Server running at http://  localhost:' + port + '/');
