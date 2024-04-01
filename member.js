68df6933ff9e82d15a8f50997b007e4db2755ff8
gattupallivk-cts <87705380+gattupallivk-cts@users.noreply.github.com>
2024-04-01 18:03:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = document.getElementsByClassName("member");
+    var i;
+    for (i = 0; i < member.length; i++) {
+        member[i].style.display = "block";
+    }
+}
